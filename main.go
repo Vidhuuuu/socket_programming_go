1ae54f3b45c39ec63e8bbb5dfe73ab31e27a067f
vidhu <vidhudash@gmail.com>
2025-04-03 03:04:48 +0530

    feat: simple tcp server

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"net"
+)
+
+const addr = "127.0.0.1:5000"
+
 func main() {
-	print("SOCKETS!\n")
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+	defer listener.Close()
+
+	log.Printf("Listening on %s...\n", addr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v\n", err)
+			continue
+		}
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("New connection from %s\n", conn.RemoteAddr())
 }
