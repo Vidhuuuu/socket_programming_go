e4670eb04335929541ec9c48a928a455dba9c01f
vidhu <vidhudash@gmail.com>
2025-04-03 23:43:24 +0530

    feat: add a basic client and modes

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	tcp "github.com/Vidhuuuu/socket_programming_go/tcp"
 )
 
 const addr = "127.0.0.1:5000"
 
 func main() {
-	tcp.StartServer(addr)
+	mode := flag.String("mode", "", "Start as '[s]erver' or '[c]lient'")
+	flag.Parse()
+
+	if *mode == "" {
+		fmt.Println("Usage: go run main.go -mode=[s|c]")
+		return
+	}
+
+	switch *mode {
+	case "s":
+		tcp.StartServer(addr)
+	case "c":
+		tcp.StartClient(addr)
+	default:
+		fmt.Printf("Unknown mode: %v\n", *mode)
+		fmt.Println("Usage: go run main.go -mode=[s|c]")
+	}
 }
--- tcp/client.go
+++ tcp/client.go
@@ -1,1 +1,33 @@
 package tcp
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func StartClient(addr string) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to connect to %v: %v\n", addr, err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Connected to server(%v). Type messages here\n", addr)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		if msg == "bye" {
+			fmt.Println("Disconnecting...")
+			break
+		}
+		fmt.Fprintln(conn, msg)
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Println("Failed to capture response from server")
+		}
+		fmt.Printf("Server: %v", response)
+	}
+}
