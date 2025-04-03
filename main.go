8959dfc2eba6f8bae972f3ba605eb7b722af55fc
vidhu <vidhudash@gmail.com>
2025-04-03 22:35:15 +0530

    refactor: add new tcp directory

--- main.go
+++ main.go
@@ -1,50 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
+	tcpServer "github.com/Vidhuuuu/socket_programming_go/tcp/tcp_server"
 )
 
 const addr = "127.0.0.1:5000"
 
 func main() {
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("Failed to listen: %v\n", err)
-	}
-	defer listener.Close()
-
-	log.Printf("Listening on %s...\n", addr)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept connection: %v\n", err)
-			continue
-		}
-		go func (c net.Conn) {
-			defer func() {
-				log.Printf("Done with %v\n", conn.RemoteAddr())
-				c.Close()
-			}()
-			handleConnection(conn)
-		}(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	log.Printf("New connection from %s\n", conn.RemoteAddr())
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		text := scanner.Text()
-		log.Printf("Received: %s\n", text)
-		fmt.Fprintf(conn, "You said: %s\n", text)
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Printf("Scanner error: %v\n", err)
-	}
+	tcpServer.TCPStartServer(addr)
 }
---
+++ tcp/tcp_server/server.go
@@ -0,0 +1,48 @@
+package tcp_server
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func TCPStartServer(addr string) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+	defer listener.Close()
+
+	log.Printf("Listening on %s...\n", addr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v\n", err)
+			continue
+		}
+		go func (c net.Conn) {
+			defer func() {
+				log.Printf("Done with %v\n", conn.RemoteAddr())
+				c.Close()
+			}()
+			handleConnection(conn)
+		}(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	log.Printf("New connection from %s\n", conn.RemoteAddr())
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		log.Printf("Received: %s\n", text)
+		fmt.Fprintf(conn, "You said: %s\n", text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Scanner error: %v\n", err)
+	}
+}
