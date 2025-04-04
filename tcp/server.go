4c8f25c81509c525f3813eea49bc84520e71b188
vidhu <vidhudash@gmail.com>
2025-04-04 18:11:03 +0530

    feat: handle server shutdown

--- tcp/server.go
+++ tcp/server.go
@@ -2,34 +2,54 @@ package tcp
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+var activeConns int
 
 func StartServer(addr string) {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
-	defer listener.Close()
 
 	log.Printf("Listening on %s...\n", addr)
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println("Failed to accept connection:", err)
-			continue
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
+					log.Println("Shutting down server...")
+					log.Println("Active connections on shutdown:", activeConns)
+					return
+				}
+				log.Println("Failed to accept connection:", err)
+				continue
+			}
+			activeConns += 1
+			go func (c net.Conn) {
+				defer func() {
+					log.Println("Done with", conn.RemoteAddr())
+					activeConns -= 1
+					c.Close()
+				}()
+				handleConnection(conn)
+			}(conn)
 		}
-		go func (c net.Conn) {
-			defer func() {
-				log.Println("Done with", conn.RemoteAddr())
-				c.Close()
-			}()
-			handleConnection(conn)
-		}(conn)
-	}
+	}()
+
+	<-stop
+	listener.Close()
 }
 
 func handleConnection(conn net.Conn) {
